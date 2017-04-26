6e4cc52a9c93bd1448fe86ef549be2819815c499
wangjian <wangjian@qhfax.com.cn>
2017-04-26 17:01:59 +0800

    提交文件

---
+++ C/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  C
+//
+//  Created by wangjian on 2017/4/26.
+//  Copyright © 2017年 com.qhfax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  C
+//
+//  Created by wangjian on 2017/4/26.
+//  Copyright © 2017年 com.qhfax. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
